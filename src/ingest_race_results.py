3f31abf50ceb90a8e855568a573bc8bb0267a68f
ramonsantz <santdevoficial@gmail.com>
2025-05-25 19:17:04 -0300

    Ajustes chamada DAG no Airflow

--- src/ingest_race_results.py
+++ src/ingest_race_results.py
@@ -1,9 +1,21 @@
 # Data ingestion
+import pandas as pd
+import os
 
+def run_ingestion():
+    url_csv = "https://raw.githubusercontent.com/toUpperCase78/formula1-datasets/refs/heads/master/Formula1_2025Season_RaceResults.csv"
+    df_race = pd.read_csv(url_csv)
+
+    output_path = os.path.join('/opt/airflow/data/raw', 'race_results_2025.csv')
+    df_race.to_csv(output_path, index=False)
+    print(f"[OK] Race results salvo em {output_path}")
+
+
+"""
 import pandas as pd
 
 url_csv = "https://raw.githubusercontent.com/toUpperCase78/formula1-datasets/refs/heads/master/Formula1_2025Season_RaceResults.csv"
 df_race = pd.read_csv(url_csv)
 
 df_race.to_csv('data/raw/race_results_2025.csv', index=False)
-
+"""
--- src/scrape_fastest_laps.py
+++ src/scrape_fastest_laps.py
@@ -3,7 +3,49 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import os
 
+def run_scraper():
+    url = 'https://www.formula1.com/en/results/2025/fastest-laps'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    table = soup.find('table', class_='f1-table')
+    rows = table.find_all('tr')[1:]
+
+    data = []
+
+    def split_driver_name(driver_text):
+        match = re.match(r"([a-zA-Z\s]+)([A-Z]{3})", driver_text)
+        if match:
+            return match.group(1), match.group(2)
+        return driver_text, ""
+
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols) > 0:
+            grand_prix = cols[0].text.strip()
+            driver_name, driver_abbr = split_driver_name(cols[1].text.strip())
+            car = cols[2].text.strip()
+            time = cols[3].text.strip()
+            data.append([grand_prix, driver_name, driver_abbr, car, time])
+
+    columns = ['Grand Prix', 'Driver', 'Driver Abbreviation', 'Car', 'Time']
+    df_fastest = pd.DataFrame(data, columns=columns)
+
+    # Caminho local (relativo ao projeto)
+    output_path_local = os.path.join('data', 'raw', 'fastest_laps_2025.csv')
+    df_fastest.to_csv(output_path_local, index=False)
+
+    print(f"[OK] Fastest laps salvo em {output_path_local}")
+
+# Executa quando o script é chamado diretamente
+if __name__ == "__main__":
+    run_scraper()
+
+
+
+"""
 url = 'https://www.formula1.com/en/results/2025/fastest-laps'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -44,3 +86,4 @@ if __name__ == '__main__':
     # Save the raw data
     df_fastest.to_csv('data/raw/fastest_laps_2025.csv', index=False)
     print(df_fastest)
+"""
