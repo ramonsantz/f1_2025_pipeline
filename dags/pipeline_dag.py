7610fbf5c0e6b01a33803ed59fab1df261d88857
ramonsantz <santdevoficial@gmail.com>
2025-06-22 17:57:42 -0300

    fix: correcao scraping e ingestao com DAG

--- dags/pipeline_dag.py
+++ dags/pipeline_dag.py
@@ -1,17 +1,29 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-import papermill as pm  # ← usamos diretamente o papermill
+import papermill as pm 
+import subprocess 
+
+def ingest_race_results():
+    subprocess.run(["python", "/opt/airflow/src/ingest_race_results.py"], check=True)
+    
+def scrape_fast_laps():
+    subprocess.run(["python", "/opt/airflow/src/scrape_fastest_laps.py"], check=True)
 
 def executar_notebook():
-    notebook_path = '/opt/airflow/src/transform_race_data.ipynb'
+    notebook_path = '/opt/airflow/notebook/transform_race_data.ipynb'
     output_path = '/opt/airflow/data/output_notebook.ipynb'
 
-    # Executa o notebook diretamente com a API papermill
+    # Executa notebook com a API papermill
     pm.execute_notebook(
         input_path=notebook_path,
         output_path=output_path,
-        parameters={}  # ← se quiser passar parâmetros para o notebook
+        parameters={
+            "input_path_race": "/opt/airflow/data/raw/race_results_2025.csv",
+            "input_path_fastest": "/opt/airflow/data/raw/fastest_laps_2025.csv",
+            "output_path_race": "/opt/airflow/data/processed/race_results_2025_clean.csv",
+            "output_path_fastest": "/opt/airflow/data/processed/fastest_laps_2025_clean.csv"
+        }  # Passar parâmetros para o notebook
     )
 
 default_args = {
@@ -24,16 +36,25 @@ default_args = {
 with DAG(
     'pipeline_dag',
     default_args=default_args,
-    description='Pipeline de dados F1 semanal',
+    description='Pipeline dados F1 semanal',
     schedule_interval='@weekly',
     start_date=datetime(2025, 5, 1),
     catchup=False,
     tags=['f1', 'pipeline'],
 ) as dag:
 
+    tarefa_ingestao_race = PythonOperator(
+        task_id='ingest_race_results',
+        python_callable=ingest_race_results
+    )
+    
+    tarefa_scrape_fastest = PythonOperator(
+        task_id='scrape_fast_laps',
+        python_callable=scrape_fast_laps
+    )
+
     tarefa_executar_notebook = PythonOperator(
         task_id='executar_notebook',
         python_callable=executar_notebook
     )
 
-    tarefa_executar_notebook
--- src/scrape_fastest_laps.py
+++ src/scrape_fastest_laps.py
@@ -1,43 +1,49 @@
-# Data ingestion
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import re
 import os
+import re
 
 def run_scraper():
     url = 'https://www.formula1.com/en/results/2025/fastest-laps'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    table = soup.find('table', class_='f1-table')
-    rows = table.find_all('tr')[1:]
+    rows = soup.select('table tbody tr')
 
     data = []
 
-    def split_driver_name(driver_text):
-        match = re.match(r"([a-zA-Z\s]+)([A-Z]{3})", driver_text)
-        if match:
-            return match.group(1), match.group(2)
-        return driver_text, ""
-
     for row in rows:
         cols = row.find_all('td')
-        if len(cols) > 0:
-            grand_prix = cols[0].text.strip()
-            driver_name, driver_abbr = split_driver_name(cols[1].text.strip())
-            car = cols[2].text.strip()
+        if len(cols) >= 4:
+            # GP
+            grand_prix_parts = cols[0].stripped_strings
+            grand_prix_list = list(grand_prix_parts)
+            grand_prix = grand_prix_list[-1] if grand_prix_list else ''
+
+            # DriverName
+            driver_span = cols[1]
+            first_name_tag = driver_span.select_one('span.max-lg\\:hidden')
+            last_name_tag = driver_span.select('span.max-md\\:hidden')
+            abbr_tag = driver_span.select_one('span.md\\:hidden')
+
+            first_name = first_name_tag.text.strip() if first_name_tag else ''
+            last_name = ''.join([tag.text.strip() for tag in last_name_tag]) if last_name_tag else ''
+            driver_full_name = f"{first_name} {last_name}".strip()
+
+            driver_abbr = abbr_tag.text.strip() if abbr_tag else ''
+
+            
+            team = cols[2].text.strip()
             time = cols[3].text.strip()
-            data.append([grand_prix, driver_name, driver_abbr, car, time])
 
-    columns = ['Grand Prix', 'Driver', 'Driver Abbreviation', 'Car', 'Time']
-    df_fastest = pd.DataFrame(data, columns=columns)
+            data.append([grand_prix, driver_full_name, driver_abbr, team, time])
 
-    # Caminho local (relativo ao projeto)
-    output_path_local = os.path.join('data', 'raw', 'fastest_laps_2025.csv')
-    df_fastest.to_csv(output_path_local, index=False)
+    df = pd.DataFrame(data, columns=['Grand Prix', 'Driver', 'Driver Abbreviation', 'Car', 'Time'])
 
-    print(f"[OK] Fastest laps salvo em {output_path_local}")
+    output_path = os.path.join('data', 'raw', 'fastest_laps_2025.csv')
+    df.to_csv(output_path, index=False)
+    print(f"[OK] Fastest laps salvo em {output_path}")
 
 if __name__ == "__main__":
     run_scraper()
